ec365ca632aacf6329afacc6ad79047502180554
Philani <philanimkalipi@gmail.com>
2025-12-01 19:02:16 +0200

    functionality issue fixed:

--- calculator.js
+++ calculator.js
@@ -1,36 +1,20 @@
 function addition(x, y) {
-    if (typeof x === 'number' && typeof y === 'number') {
-        var sum = x + y;
-        return sum;
-    }
-    return -1;
+    return x + y;
 }
 function substraction(x, y) {
-    if (typeof x === 'number' && typeof y === 'number') {
-        var sum = x - y;
-        return sum;
-    }
-    return -1;
+    return x - y;
 }
 function multiplication(x, y) {
-    if (typeof x === 'number' && typeof y === 'number') {
-        var sum = x * y;
-        return sum;
-    }
-    return -1;
+    return x * y;
 }
 function division(x, y) {
-    if (typeof x === 'number' && typeof y === 'number') {
-        var sum = x / y;
-        return sum;
-    }
-    return -1;
+    return x / y;
 }
 function calc(x, y, symbol) {
     if (symbol === "+") {
         return addition(x, y);
     }
-    if (symbol === "*") {
+    if (symbol === "*" || symbol === "x") {
         return multiplication(x, y);
     }
     if (symbol === "/") {
@@ -39,113 +23,57 @@ function calc(x, y, symbol) {
     if (symbol === "-") {
         return substraction(x, y);
     }
+    return 0;
 }
-var tempNumber = ""; // temporarily stores the current numerical value until operator is clicked
-var finalList = []; // stores the numbers and operators that are to be used to get the answer when you press '='
+var tempNumber = "";
+var finalList = [];
 var miniDisplay = "";
 document.addEventListener("DOMContentLoaded", function () {
     var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t;
     console.log("done");
-    // output display
     var display = document.querySelector('.displayText');
-    // number buttons
-    (_a = document.querySelector('#one')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function (event) {
+    // Helper function to handle number inputs
+    var handleNumberClick = function (event) {
         event.preventDefault();
         var target = event.target;
         tempNumber = tempNumber + target.value;
         console.log(tempNumber);
         display.value = tempNumber;
-    });
-    (_b = document.querySelector('#two')) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_c = document.querySelector('#three')) === null || _c === void 0 ? void 0 : _c.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_d = document.querySelector('#four')) === null || _d === void 0 ? void 0 : _d.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_e = document.querySelector('#five')) === null || _e === void 0 ? void 0 : _e.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_f = document.querySelector('#six')) === null || _f === void 0 ? void 0 : _f.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_g = document.querySelector('#seven')) === null || _g === void 0 ? void 0 : _g.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_h = document.querySelector('#eight')) === null || _h === void 0 ? void 0 : _h.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_j = document.querySelector('#nine')) === null || _j === void 0 ? void 0 : _j.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_k = document.querySelector('#zero')) === null || _k === void 0 ? void 0 : _k.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    (_l = document.querySelector('#fullstop')) === null || _l === void 0 ? void 0 : _l.addEventListener("click", function (event) {
-        event.preventDefault();
-        var target = event.target;
-        tempNumber = tempNumber + target.value;
-        console.log(tempNumber);
-        display.value = tempNumber;
-    });
-    // clear and delete functionality
+    };
+    // Number buttons
+    (_a = document.querySelector('#one')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", handleNumberClick);
+    (_b = document.querySelector('#two')) === null || _b === void 0 ? void 0 : _b.addEventListener("click", handleNumberClick);
+    (_c = document.querySelector('#three')) === null || _c === void 0 ? void 0 : _c.addEventListener("click", handleNumberClick);
+    (_d = document.querySelector('#four')) === null || _d === void 0 ? void 0 : _d.addEventListener("click", handleNumberClick);
+    (_e = document.querySelector('#five')) === null || _e === void 0 ? void 0 : _e.addEventListener("click", handleNumberClick);
+    (_f = document.querySelector('#six')) === null || _f === void 0 ? void 0 : _f.addEventListener("click", handleNumberClick);
+    (_g = document.querySelector('#seven')) === null || _g === void 0 ? void 0 : _g.addEventListener("click", handleNumberClick);
+    (_h = document.querySelector('#eight')) === null || _h === void 0 ? void 0 : _h.addEventListener("click", handleNumberClick);
+    (_j = document.querySelector('#nine')) === null || _j === void 0 ? void 0 : _j.addEventListener("click", handleNumberClick);
+    (_k = document.querySelector('#zero')) === null || _k === void 0 ? void 0 : _k.addEventListener("click", handleNumberClick);
+    (_l = document.querySelector('#fullstop')) === null || _l === void 0 ? void 0 : _l.addEventListener("click", handleNumberClick);
+    // Clear and delete functionality
     (_m = document.querySelector('#clear')) === null || _m === void 0 ? void 0 : _m.addEventListener("click", function (event) {
         event.preventDefault();
-        var target = event.target;
         tempNumber = "";
+        finalList = [];
+        miniDisplay = "";
         console.log(tempNumber);
         display.value = "0";
     });
     (_o = document.querySelector('#delete')) === null || _o === void 0 ? void 0 : _o.addEventListener("click", function (event) {
         event.preventDefault();
-        var target = event.target;
         tempNumber = tempNumber.slice(0, -1);
         console.log(tempNumber);
-        display.value = tempNumber;
+        display.value = tempNumber || "0";
     });
-    //operators functionality
-    (_p = document.querySelector('#plus')) === null || _p === void 0 ? void 0 : _p.addEventListener("click", function (event) {
+    // Helper function for operators
+    var handleOperator = function (event) {
         event.preventDefault();
-        var n = tempNumber;
-        finalList.push(n);
+        if (tempNumber === "")
+            return; // Don't add operator if no number entered
+        var num = parseFloat(tempNumber);
+        finalList.push(num);
         miniDisplay = miniDisplay + tempNumber;
         tempNumber = "";
         display.value = "0";
@@ -153,74 +81,39 @@ document.addEventListener("DOMContentLoaded", function () {
         finalList.push(symbol.value);
         miniDisplay = miniDisplay + symbol.value;
         console.log(miniDisplay);
-    });
-    (_q = document.querySelector('#times')) === null || _q === void 0 ? void 0 : _q.addEventListener("click", function (event) {
-        event.preventDefault();
-        var n = tempNumber;
-        finalList.push(n);
-        miniDisplay = miniDisplay + tempNumber;
-        tempNumber = "";
-        display.value = "0";
-        var symbol = event.target;
-        finalList.push(symbol.value);
-        miniDisplay = miniDisplay + symbol.value;
-        console.log(miniDisplay);
-    });
-    (_r = document.querySelector('#minus')) === null || _r === void 0 ? void 0 : _r.addEventListener("click", function (event) {
-        event.preventDefault();
-        var n = tempNumber;
-        finalList.push(n);
-        miniDisplay = miniDisplay + tempNumber;
-        tempNumber = "";
-        display.value = "0";
-        var symbol = event.target;
-        finalList.push(symbol.value);
-        miniDisplay = miniDisplay + symbol.value;
-        console.log(miniDisplay);
-    });
-    (_s = document.querySelector('#divide')) === null || _s === void 0 ? void 0 : _s.addEventListener("click", function (event) {
-        event.preventDefault();
-        var n = tempNumber;
-        finalList.push(n);
-        miniDisplay = miniDisplay + tempNumber;
-        tempNumber = "";
-        display.value = "0";
-        var symbol = event.target;
-        finalList.push(symbol.value);
-        miniDisplay = miniDisplay + symbol.value;
-        console.log(miniDisplay);
-    });
-    // equals to operator functionality
+    };
+    // Operators functionality
+    (_p = document.querySelector('#plus')) === null || _p === void 0 ? void 0 : _p.addEventListener("click", handleOperator);
+    (_q = document.querySelector('#times')) === null || _q === void 0 ? void 0 : _q.addEventListener("click", handleOperator);
+    (_r = document.querySelector('#minus')) === null || _r === void 0 ? void 0 : _r.addEventListener("click", handleOperator);
+    (_s = document.querySelector('#divide')) === null || _s === void 0 ? void 0 : _s.addEventListener("click", handleOperator);
+    // Equals operator functionality
     (_t = document.querySelector('#equalto')) === null || _t === void 0 ? void 0 : _t.addEventListener("click", function (event) {
         event.preventDefault();
         console.log("clicked =");
-        var n = tempNumber;
-        finalList.push(n);
-        // iterate through the list and perform operations
-        var counter = 0;
-        var num_1 = 0;
-        var num_2 = 0;
-        var currSymbol = "";
-        var answer = 0;
-        for (var i = 0; i < finalList.length - 1; i++) {
-            if (typeof finalList[i] === 'number' && counter === 0) {
-                num_1 = finalList[i];
-                counter = counter + 1;
-            }
-            if (typeof finalList[i] === 'string' && counter === 1) {
-                currSymbol = finalList[i];
-                counter = counter + 1;
-            }
-            if (typeof finalList[i] === 'number' && counter === 0) {
-                num_2 = finalList[i];
-                counter = 1;
-                if (typeof num_1 === 'number' && typeof num_2 === 'number' && typeof currSymbol === 'string') {
-                    answer = calc(num_1, num_2, currSymbol);
-                }
+        if (tempNumber !== "") {
+            var num = parseFloat(tempNumber);
+            finalList.push(num);
+        }
+        console.log("Final list:", finalList);
+        // Calculate result
+        if (finalList.length === 0) {
+            display.value = "0";
+            return;
+        }
+        var result = finalList[0];
+        for (var i = 1; i < finalList.length; i += 2) {
+            var operator = finalList[i];
+            var nextNum = finalList[i + 1];
+            if (nextNum !== undefined) {
+                result = calc(result, nextNum, operator);
             }
         }
-        var str = answer;
-        console.log(str);
-        display.value = str;
+        console.log("Result:", result);
+        display.value = result.toString();
+        // Reset for next calculation
+        tempNumber = "";
+        finalList = [];
+        miniDisplay = "";
     });
 });
--- calculator.ts
+++ calculator.ts
@@ -1,235 +1,141 @@
-
-function addition (x: finalListValues, y:finalListValues) {
-  if(typeof x === 'number' && typeof y === 'number') {
-     const sum: number = x + y;
-     return sum
-  }
-  return -1
-}
-function substraction (x: number, y:number) {
-    if(typeof x === 'number' && typeof y === 'number') {
-     const sum: number = x - y;
-     return sum
-  }
-  return -1
-}
-function multiplication (x: number, y:number) {
-    if(typeof x === 'number' && typeof y === 'number') {
-     const sum: number = x * y;
-     return sum
-  }
-  return -1
-}
-function division (x: number, y:number) {
-    if(typeof x === 'number' && typeof y === 'number') {
-     const sum: number = x / y;
-     return sum
-  }
-  return -1
-}
-function calc (x: number, y:number, symbol: string) {
-    if(symbol === "+") {
-        return addition(x, y)
-    }
-    if(symbol === "*") {
-        return multiplication(x, y)
-    }
-    if(symbol === "/") {
-        return division(x, y)
-    }
-    if(symbol === "-") {
-        return substraction(x, y)
-    }
+function addition(x: number, y: number): number {
+  return x + y;
 }
 
+function substraction(x: number, y: number): number {
+  return x - y;
+}
 
-let tempNumber: string = ""; // temporarily stores the current numerical value until operator is clicked
-type finalListValues = string | number; 
-let finalList: finalListValues[] = []; // stores the numbers and operators that are to be used to get the answer when you press '='
+function multiplication(x: number, y: number): number {
+  return x * y;
+}
+
+function division(x: number, y: number): number {
+  return x / y;
+}
+
+function calc(x: number, y: number, symbol: string): number {
+  if (symbol === "+") {
+    return addition(x, y);
+  }
+  if (symbol === "*" || symbol === "x") {
+    return multiplication(x, y);
+  }
+  if (symbol === "/") {
+    return division(x, y);
+  }
+  if (symbol === "-") {
+    return substraction(x, y);
+  }
+  return 0;
+}
+
+let tempNumber: string = "";
+type finalListValues = string | number;
+let finalList: finalListValues[] = [];
 let miniDisplay: string = "";
 
 document.addEventListener("DOMContentLoaded", () => {
- console.log("done");
- // output display
- const display = document.querySelector('.displayText') as HTMLInputElement;
+  console.log("done");
+  const display = document.querySelector('.displayText') as HTMLInputElement;
 
- // number buttons
- document.querySelector('#one')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value ;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
- document.querySelector('#two')?.addEventListener("click", ( event: Event) => {
+  // Helper function to handle number inputs
+  const handleNumberClick = (event: Event) => {
     event.preventDefault();
     const target = event.target as HTMLInputElement;
     tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
+    console.log(tempNumber);
     display.value = tempNumber;
- })
-  document.querySelector('#three')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#four')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#five')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#six')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#seven')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#eight')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#nine')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
-  document.querySelector('#zero')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
- document.querySelector('#fullstop')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber + target.value;
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
+  };
 
- // clear and delete functionality
-  document.querySelector('#clear')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = "";
-    console.log(tempNumber)
-    display.value = "0";
- })
-  document.querySelector('#delete')?.addEventListener("click", ( event: Event) => {
-    event.preventDefault();
-    const target = event.target as HTMLInputElement;
-    tempNumber = tempNumber.slice(0,-1);
-    console.log(tempNumber)
-    display.value = tempNumber;
- })
+  // Number buttons
+  document.querySelector('#one')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#two')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#three')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#four')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#five')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#six')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#seven')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#eight')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#nine')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#zero')?.addEventListener("click", handleNumberClick);
+  document.querySelector('#fullstop')?.addEventListener("click", handleNumberClick);
 
- //operators functionality
- document.querySelector('#plus')?.addEventListener("click", (event: Event) => {
-    event.preventDefault()
-    let n:unknown = tempNumber;
-    finalList.push(n as number);
-    miniDisplay = miniDisplay + tempNumber;
-    tempNumber = "";
-    display.value = "0";
-    const symbol = event.target as HTMLInputElement;
-    finalList.push(symbol.value)
-    miniDisplay = miniDisplay + symbol.value;
-    console.log(miniDisplay)
- })
- document.querySelector('#times')?.addEventListener("click", (event: Event) => {
-    event.preventDefault()
-    let n:unknown = tempNumber;
-    finalList.push(n as number);
-    miniDisplay = miniDisplay + tempNumber;
-    tempNumber = "";
-    display.value = "0";
-    const symbol = event.target as HTMLInputElement;
-    finalList.push(symbol.value)
-    miniDisplay = miniDisplay + symbol.value;
-    console.log(miniDisplay)
- })
- document.querySelector('#minus')?.addEventListener("click", (event: Event) => {
-    event.preventDefault()
-    let n:unknown = tempNumber;
-    finalList.push(n as number);
-    miniDisplay = miniDisplay + tempNumber;
-    tempNumber = "";
-    display.value = "0";
-    const symbol = event.target as HTMLInputElement;
-    finalList.push(symbol.value)
-    miniDisplay = miniDisplay + symbol.value;
-    console.log(miniDisplay)
- })
- document.querySelector('#divide')?.addEventListener("click", (event: Event) => {
-    event.preventDefault()
-    let n:unknown = tempNumber;
-    finalList.push(n as number);
-    miniDisplay = miniDisplay + tempNumber;
-    tempNumber = "";
-    display.value = "0";
-    const symbol = event.target as HTMLInputElement;
-    finalList.push(symbol.value)
-    miniDisplay = miniDisplay + symbol.value;
-    console.log(miniDisplay)
- })
- // equals to operator functionality
- document.querySelector('#equalto')?.addEventListener("click", (event: Event) => {
+  // Clear and delete functionality
+  document.querySelector('#clear')?.addEventListener("click", (event: Event) => {
     event.preventDefault();
-    console.log("clicked =")
-    let n:unknown = tempNumber;
-    finalList.push(n as number);
-    // iterate through the list and perform operations
-    let counter = 0;
-    let num_1: finalListValues = 0;
-    let num_2: finalListValues = 0;
-    let currSymbol: finalListValues = "";
-    type ans = number | undefined;
-    let answer: ans = 0;
-    for(let i:number = 0; i<finalList.length-1; i++){
-        if(typeof finalList[i] === 'number' && counter === 0){
-            num_1 = finalList[i];
-            counter = counter + 1;
-        }
-        if(typeof finalList[i] === 'string' && counter === 1){
-            currSymbol = finalList[i];
-            counter = counter + 1;
-        }
-        if(typeof finalList[i] === 'number' && counter === 0){
-            num_2 = finalList[i];
-            counter = 1;
-            if (typeof num_1 === 'number' && typeof num_2 === 'number' && typeof currSymbol === 'string') {
-               answer = calc( num_1, num_2, currSymbol); 
-            }
-        
-        }
+    tempNumber = "";
+    finalList = [];
+    miniDisplay = "";
+    console.log(tempNumber);
+    display.value = "0";
+  });
+
+  document.querySelector('#delete')?.addEventListener("click", (event: Event) => {
+    event.preventDefault();
+    tempNumber = tempNumber.slice(0, -1);
+    console.log(tempNumber);
+    display.value = tempNumber || "0";
+  });
+
+  // Helper function for operators
+  const handleOperator = (event: Event) => {
+    event.preventDefault();
+    if (tempNumber === "") return; // Don't add operator if no number entered
+    
+    const num = parseFloat(tempNumber);
+    finalList.push(num);
+    miniDisplay = miniDisplay + tempNumber;
+    tempNumber = "";
+    display.value = "0";
+    
+    const symbol = event.target as HTMLInputElement;
+    finalList.push(symbol.value);
+    miniDisplay = miniDisplay + symbol.value;
+    console.log(miniDisplay);
+  };
+
+  // Operators functionality
+  document.querySelector('#plus')?.addEventListener("click", handleOperator);
+  document.querySelector('#times')?.addEventListener("click", handleOperator);
+  document.querySelector('#minus')?.addEventListener("click", handleOperator);
+  document.querySelector('#divide')?.addEventListener("click", handleOperator);
+
+  // Equals operator functionality
+  document.querySelector('#equalto')?.addEventListener("click", (event: Event) => {
+    event.preventDefault();
+    console.log("clicked =");
+    
+    if (tempNumber !== "") {
+      const num = parseFloat(tempNumber);
+      finalList.push(num);
     }
-    let str: unknown = answer;
-    console.log(str as string)
-    display.value = str as string;
- })
-})
+    
+    console.log("Final list:", finalList);
+    
+    // Calculate result
+    if (finalList.length === 0) {
+      display.value = "0";
+      return;
+    }
+    
+    let result = finalList[0] as number;
+    
+    for (let i = 1; i < finalList.length; i += 2) {
+      const operator = finalList[i] as string;
+      const nextNum = finalList[i + 1] as number;
+      
+      if (nextNum !== undefined) {
+        result = calc(result, nextNum, operator);
+      }
+    }
+    
+    console.log("Result:", result);
+    display.value = result.toString();
+    
+    // Reset for next calculation
+    tempNumber = "";
+    finalList = [];
+    miniDisplay = "";
+  });
+});
